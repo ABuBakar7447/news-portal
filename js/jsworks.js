c1fae6feaf5bf61b0b6ff3789138d0eb3a3a7f7a
ABuBakar7447 <lone7447wolf@gmail.com>
2022-09-03 10:53:59 +0600

    newsdetails part

--- js/jsworks.js
+++ js/jsworks.js
@@ -24,11 +24,28 @@ const addNews = news => {
         
         newsDiv.innerHTML = `
         
-        <a class="nav-link fw-bold text-dark" href="#" onclick="">${neWs.category_name}</a>
+        <a class="nav-link fw-bold text-dark" href="#" oonclick="loadNewsDetails(${neWs.category_id})">${neWs.category_name}</a>
+        
         `;
         newsTopic.appendChild(newsDiv);
 
     });
+}
+
+const loadNewsDetails = async (id) => {
+
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+    }
+
+    catch (error) {
+        console.log('there is something fishy')
+
+    }
+
 }
 
 
